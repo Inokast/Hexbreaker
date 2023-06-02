718c1b279be0183b7251f0df477c7cc917fda20f
ArcChelon <114192814+ArcChelon@users.noreply.github.com>
2023-06-01 23:52:32 -0400

    EnemyOutlineWithUIpopUp

---
+++ Assets/Test_Connor/ConnorSight.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnorSight : MonoBehaviour
+{
+    [SerializeField] private Material blank;
+    [SerializeField] private Material outlineMaterial;
+    [SerializeField] private string selectableTag = "Enemy";
+    [SerializeField] public CanvasGroup worldCanvasGroup;
+
+   
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection !=null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = blank;
+            _selection = null;
+            worldCanvasGroup.alpha =0;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = outlineMaterial;
+                    worldCanvasGroup.alpha =1 ;
+                }
+                _selection = selection;
+            }
+            
+        }
+        
+    }
+}
