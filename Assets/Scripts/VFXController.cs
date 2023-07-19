5ea1e5af0fa4115a29cc4b9aeb584747635da814
ArcChelon <114192814+ArcChelon@users.noreply.github.com>
2023-07-19 16:32:14 -0400

    edit

--- Assets/Scripts/VFXController.cs
+++ Assets/Scripts/VFXController.cs
@@ -9,7 +9,8 @@ public class VFXController : MonoBehaviour
     [SerializeField] private VisualEffect QTEtiming;
     [SerializeField] private VisualEffect HexBreak;
     [SerializeField] private VisualEffect QTEburst;
-    //  [SerializeField] private VisualEffect QTEsequence;
+    //  [SerializeField] private VisualEffect QTEarray;
+    //  [SerializeField] private VisualEffect QTEstandard;
     // [SerializeField] private VisualEffect QTEfocus;
     //[SerializeField] private VisualEffect QTEcharging;
 
@@ -23,20 +24,34 @@ public class VFXController : MonoBehaviour
         
     }
 
+    public void PlayParticleBurst()
+    {
+        VisualEffect particle = Instantiate(QTEburst, BurstButton.transform.position, BurstButton.transform.rotation);
+        particle.Play();
 
-    public void PlayParticleSpam()
+    }
+
+    public void PlayParticleMash()
         {
-       // VisualEffect particle = Instantiate(QTEspam, EnemyBattleStation1.transform.position, EnemyBattleStation1.transform.rotation);
+        // VisualEffect particle = Instantiate(QTEspam, EnemyBattleStation1.transform.position, EnemyBattleStation1.transform.rotation);
         //particle.Play();
       
         }
-    public void PlayParticleBurst()
+
+    public void PlayParticleTimed()
     {
-        VisualEffect particle = Instantiate(QTEburst,BurstButton.transform.position, BurstButton.transform.rotation);
-        particle.Play();
-        
+        // QTEtiming
     }
 
+    public void PlayParticleArray()
+    {
+        // QTEarray
+    }
+
+    public void PlayParticleStandard()
+    {
+
+    }
 
     public void KillParticles()
     {
